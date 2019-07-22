b73c288f8262485d15f85d0f08fb8b1b8183fcb2
Florian La Roche <Florian.LaRoche@gmail.com>
2019-07-22 20:55:33 +0200

    further improve kernel compile scripts

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -8,21 +8,34 @@
 sudo apt install build-essential fakeroot rsync git
 sudo apt build-dep linux
 
-# Extract the raspberry-pi patches into a subdirectory:
-if ! test -d rpi-patches-4.19.58 ; then
+KVER=4.19.37
+
+# Should we apply the raspberry-pi kernel patches?
+RPIPATCHES=0
+if test "X$HOSTTYPE" != "Xx86_64" ; then
+  RPIPATCHES=1
+  #RVER=$KVER
+  RVER=4.19.58
+fi
+
+if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
+  # Extract the raspberry-pi patches into a subdirectory:
   git clone -b rpi-4.19.y https://github.com/raspberrypi/linux/ rpi-linux
   cd rpi-linux
-  git format-patch -o ../rpi-patches-4.19.58 7a6bfa08b938d33ba0a2b80d4f717d4f0dbf9170
+  git format-patch -o ../rpi-patches-$RVER 7a6bfa08b938d33ba0a2b80d4f717d4f0dbf9170
   cd ..
 fi
 
-git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git
-test -f orig/linux_4.19.37.orig.tar.xz || wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.19.37.tar.xz
+if ! test -d linux ; then
+  git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git
+fi
+#exit 0
+test -f orig/linux_$KVER.orig.tar.xz || wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-$KVER.tar.xz
 cd linux
-test -f ../orig/linux_4.19.37.orig.tar.xz || XZ_DEFAULTS="-T 0" debian/bin/genorig.py ../linux-4.19.37.tar.xz
+test -f ../orig/linux_$KVER.orig.tar.xz || XZ_DEFAULTS="-T 0" debian/bin/genorig.py ../linux-$KVER.tar.xz
 # Just to safe disk space and have a faster compile:
 sed -i -e 's/^debug-info: true/debug-info: false/g' debian/config/defines
-if test "X$HOSTTYPE" != "Xx86_64" ; then
+if test "$RPIPATCHES" = 1 ; then
   pushd debian/patches
     mkdir bugfix/rpi
     cp ../../../rpi-patches-4.19.49/*.patch bugfix/rpi/
--- vmdb2-debian/kernel5.sh
+++ vmdb2-debian/kernel5.sh
@@ -8,24 +8,37 @@
 sudo apt install build-essential fakeroot rsync git
 sudo apt build-dep linux
 
-# Extract the raspberry-pi patches into a subdirectory:
-if ! test -d rpi-patches-5.2.1 ; then
+KVER=5.2.1
+
+# Should we apply the raspberry-pi kernel patches?
+RPIPATCHES=0
+if test "X$HOSTTYPE" != "Xx86_64" ; then
+  RPIPATCHES=1
+  RVER=$KVER
+  #RVER=5.2.1
+fi
+
+if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
+  # Extract the raspberry-pi patches into a subdirectory:
   git clone -b rpi-5.2.y https://github.com/raspberrypi/linux/ rpi-linux-5
   cd rpi-linux-5
-  git format-patch -o ../rpi-patches-5.2.1 527a3db363a3bd7e6ae0a77da809e01847a9931c
+  git format-patch -o ../rpi-patches-$RVER 527a3db363a3bd7e6ae0a77da809e01847a9931c
   cd ..
 fi
 
-git clone --single-branch --depth 1 https://salsa.debian.org/kernel-team/linux.git linux-5
-test -f orig/linux_5.2.1.orig.tar.xz || wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.2.1.tar.xz
+if ! test -d linux-5 ; then
+  git clone --single-branch --depth 1 https://salsa.debian.org/kernel-team/linux.git linux-5
+fi
+#exit 0
+test -f orig/linux_$KVER.orig.tar.xz || wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$KVER.tar.xz
 cd linux-5
-test -f ../orig/linux_5.2.1.orig.tar.xz || XZ_DEFAULTS="-T 0" debian/bin/genorig.py ../linux-5.2.1.tar.xz
+test -f ../orig/linux_$KVER.orig.tar.xz || XZ_DEFAULTS="-T 0" debian/bin/genorig.py ../linux-$KVER.tar.xz
 # Just to safe disk space and have a faster compile:
 sed -i -e 's/^debug-info: true/debug-info: false/g' debian/config/defines
-if test "X$HOSTTYPE" != "Xx86_64" ; then
+if test "$RPIPATCHES" = 1 ; then
   pushd debian/patches
     mkdir bugfix/rpi
-    cp ../../../rpi-patches-5.2.1/*.patch bugfix/rpi/
+    cp ../../../rpi-patches-$RVER/*.patch bugfix/rpi/
     ls bugfix/rpi/*.patch >> series
   popd
   # Current 5.2.y does not compile with CONFIG_RTL8192CU
