d4860a3e33dd949dc824411c8157126d8cdb8f38
Florian La Roche <Florian.LaRoche@gmail.com>
2019-08-03 06:30:31 +0200

    update kernel version number to 5.2.5, add comment to virt-copy

--- vmdb2-debian/install.sh
+++ vmdb2-debian/install.sh
@@ -38,8 +38,10 @@ if ! test -f "$DISK" ; then
     #sudo virt-ls -l -a "$DISK" /
     sudo virt-copy-in -a "$DISK" setup.sh /root/
   fi
-  if test -f linux-image-5.2.0-trunk-amd64-unsigned_5.2.2-1~exp1_amd64.deb ; then
-    sudo virt-copy-in -a "$DISK" linux-image-5.2.0-trunk-amd64-unsigned_5.2.2-1~exp1_amd64.deb /root/
+  # If you later on want to use virt-copy and the command does not recognize the
+  # correct root partition, you might have to add this param: -m /dev/sda1
+  if test -f linux-image-5.2.0-trunk-amd64-unsigned_5.2.5-1~exp1_amd64.deb ; then
+    sudo virt-copy-in -a "$DISK" linux-image-5.2.0-trunk-amd64-unsigned_5.2.5-1~exp1_amd64.deb /root/
   fi
   virt-install --name "$TARGET" --memory 4096 --cpu host --vcpus 4 --boot hd --disk "$DISK"
   # --os-variant debiansqueeze
