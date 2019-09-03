ddecaead71d129a54e9b2c9ae8d5c64bd6a89e6c
Florian La Roche <Florian.LaRoche@gmail.com>
2019-09-03 18:50:16 +0200

    kernel.sh: merge in changes from kernel5.sh, try compiling newer 4.19.x rpi kernels

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -3,32 +3,70 @@
 # Recompile a debian kernel. On arm apply the raspberry-pi kernel
 # patches to the debian kernel.
 #
-
-# Build requirements:
-sudo apt -q -y install build-essential fakeroot rsync git
-sudo apt -q -y build-dep linux
-
-KVER=4.19.37
+# The following params are possible:
+# - no param: compile for the current arch a current Debian kernel
+# - "armhf": cross-compile a current generic armhf Debian kernel
+# - "arm64": cross-compile a current generic arm64 Debian kernel
+# - "rpi-armhf": cross-compile a current armhf Debian kernel including all raspberry-pi patches
+# - "rpi-arm64": cross-compile a current arm64 Debian kernel including all raspberry-pi patches
+#
+# https://wiki.debian.org/HowToCrossBuildAnOfficialDebianKernelPackage
+#
 
 # Should we apply the raspberry-pi kernel patches?
 RPIPATCHES=0
 if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
+fi
+
+CROSS=0
+ARCH=
+if test "X$1" = "Xarm64" -o "X$1" = "Xrpi-arm64" ; then
+  CROSS=1
+  ARCH=arm64
+  if test "X$1" = "Xrpi-arm64" ; then
+    RPIPATCHES=1
+  fi
+fi
+if test "X$1" = "Xarmhf" -o "X$1" = "Xrpi-armhf" ; then
+  CROSS=1
+  ARCH=armhf
+  if test "X$1" = "Xrpi-armhf" ; then
+    RPIPATCHES=1
+  fi
+fi
+
+# Build requirements:
+if true ; then
+sudo apt -q -y install build-essential fakeroot rsync git #python-debian python3-debian
+sudo apt -q -y build-dep linux
+if test $CROSS = 1 ; then
+  sudo apt -q -y install kernel-wedge quilt ccache flex bison libssl-dev
+  sudo apt -q -y install g++-8-aarch64-linux-gnu g++-8-arm-linux-gnueabihf
+  #sudo apt -q -y install crossbuild-essential-arm64 crossbuild-essential-armhf
+fi
+fi
+
+KVER=4.19.67
+
+if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
-  RVER=4.19.58
+  RVER=4.19.69
 fi
 
 if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
   # Extract the raspberry-pi patches into a subdirectory:
-  git clone -b rpi-4.19.y https://github.com/raspberrypi/linux/ rpi-linux
+  if test ! -d rpi-linux ; then
+    git clone -b rpi-4.19.y https://github.com/raspberrypi/linux/ rpi-linux
+  fi
   cd rpi-linux
-  git format-patch -o ../rpi-patches-$RVER 7a6bfa08b938d33ba0a2b80d4f717d4f0dbf9170
+  git format-patch -o ../rpi-patches-$RVER 97ab07e11fbf55c86c3758e07ab295028bf17f94
   cd ..
   #rm -fr rpi-linux
 fi
 
 if ! test -d linux ; then
-  git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git
+  git clone --single-branch --depth 1 -b buster https://salsa.debian.org/kernel-team/linux.git
 fi
 #exit 0
 test -f orig/linux_$KVER.orig.tar.xz || wget -q https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-$KVER.tar.xz
@@ -39,15 +77,42 @@ sed -i -e 's/^debug-info: true/debug-info: false/g' debian/config/defines
 if test "$RPIPATCHES" = 1 ; then
   pushd debian/patches
     mkdir bugfix/rpi
-    cp ../../../rpi-patches-4.19.49/*.patch bugfix/rpi/
-    rm -f bugfix/rpi/0506-Bluetooth-Check-key-sizes-only-when-Secure-Simple-Pa.patch
+    cp ../../../rpi-patches-$RVER/*.patch bugfix/rpi/
     ls bugfix/rpi/*.patch >> series
   popd
+  rm -f debian/abi/4.19.0-6/arm*
 fi
+
+if test $CROSS = 0 ; then
+
 debian/rules orig
 debian/rules debian/control
 PAR="$(grep -c ^processor /proc/cpuinfo)"
 #PAR=10
 DEB_BUILD_OPTIONS="parallel=$PAR" XZ_DEFAULTS="-T 0" fakeroot debian/rules binary-arch 2>&1 | tee LOG
+
+else
+
+export $(dpkg-architecture -a$ARCH)
+export PATH=/usr/lib/ccache:$PATH
+# Build profiles is from: https://salsa.debian.org/kernel-team/linux/blob/master/debian/README.source
+export DEB_BUILD_PROFILES="cross nopython nodoc pkg.linux.notools"
+# Enable build in parallel
+export MAKEFLAGS="-j$(($(nproc)*2))"
+# Disable -dbg (debug) package is only possible when distribution="UNRELEASED" in debian/changelog
+export DEBIAN_KERNEL_DISABLE_DEBUG=
+[ "$(dpkg-parsechangelog --show-field Distribution)" = "UNRELEASED" ] &&
+  export DEBIAN_KERNEL_DISABLE_DEBUG=yes
+
+fakeroot make -f debian/rules clean
+fakeroot make -f debian/rules orig
+fakeroot make -f debian/rules source
+fakeroot make -f debian/rules.gen setup_${ARCH}
+sed -i 's/binary-arch_arm64:: binary-arch_arm64_none binary-arch_arm64_real/binary-arch_arm64:: binary-arch_arm64_none/' debian/rules.gen
+sed -i 's/binary-arch_armhf:: binary-arch_armhf_extra binary-arch_armhf_none binary-arch_armhf_real/binary-arch_armhf:: binary-arch_armhf_extra binary-arch_armhf_none/' debian/rules.gen
+fakeroot make -f debian/rules.gen binary-arch_${ARCH}
+
+fi
+
 cd ..
 
