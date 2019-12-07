1518c712bcc2e00369b3f31ced4b806a26b2264b
Florian La Roche <Florian.LaRoche@gmail.com>
2019-12-07 23:24:52 +0100

    setup.sh: add mesa-utils and inxi to GUI installs

--- vmdb2-debian/setup.sh
+++ vmdb2-debian/setup.sh
@@ -92,7 +92,7 @@ if false ; then
   tasksel install gnome-desktop --new-install
   $apt install firefox-esr firefox-esr-l10n-de chromium chromium-l10n vlc
   $apt install libreoffice libreoffice-help-de libreoffice-l10n-de
-  $apt install rdesktop dconf-editor imagemagick
+  $apt install rdesktop dconf-editor imagemagick mesa-utils inxi
   # https://superuser.com/questions/394376/how-to-prevent-gnome-shells-alttab-from-grouping-windows-from-similar-apps
   #dconf write /org/gnome/desktop/wm/keybindings/switch-applications
   #dconf write /org/gnome/desktop/wm/keybindings/switch-applications-backward
