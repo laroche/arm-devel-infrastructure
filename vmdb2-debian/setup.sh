51fd546548c69e794e6358fdfcd80eb8e01103e8
Florian La Roche <Florian.LaRoche@gmail.com>
2019-12-28 09:21:47 +0100

    setup.sh: support Debian testing, add network-manager-openconnect-gnome,libguestfs-tools,Google chrome,visual studio code,better wine support,pylint/pyflakes, update to qemu-4.2.0

--- vmdb2-debian/setup.sh
+++ vmdb2-debian/setup.sh
@@ -21,6 +21,10 @@ unstable="0"
 if grep -q unstable /etc/apt/sources.list ; then
   unstable="1"
 fi
+testing="0"
+if grep -q testing /etc/apt/sources.list ; then
+  testing="1"
+fi
 
 if false ; then
 if test -b /dev/sda && ! test -b /dev/sda2 ; then
@@ -59,7 +63,7 @@ sed -i -e 's/^%sudo.*/%sudo\tALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers
 # https://unix.stackexchange.com/questions/318824/vim-cutpaste-not-working-in-stretch-debian-9
 # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=864074
 # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=837761#76
-if test $unstable = 1 ; then
+if test $unstable = 1 -o $testing = 1 ; then
   sed -i -e '/has.*mouse/,+6s/^/"/' /usr/share/vim/vim81/defaults.vim
 else
   sed -i -e '/has.*mouse/,+2s/^/"/' /usr/share/vim/vim81/defaults.vim
@@ -93,6 +97,7 @@ if false ; then
   $apt install firefox-esr firefox-esr-l10n-de chromium chromium-l10n vlc
   $apt install libreoffice libreoffice-help-de libreoffice-l10n-de
   $apt install rdesktop dconf-editor imagemagick mesa-utils inxi
+  $apt install network-manager-openconnect-gnome
   # https://superuser.com/questions/394376/how-to-prevent-gnome-shells-alttab-from-grouping-windows-from-similar-apps
   #dconf write /org/gnome/desktop/wm/keybindings/switch-applications
   #dconf write /org/gnome/desktop/wm/keybindings/switch-applications-backward
@@ -103,9 +108,17 @@ if false ; then
   #gsettings set org.gnome.shell.window-switcher current-workspace-only true
 
   # virtualization support:
-  $apt install virtinst virt-manager
+  $apt install virtinst virt-manager libguestfs-tools
 
   $apt install qemu-system-arm qemu-efi minicom
+
+  # Google chrome browser: (https://wiki.debian.org/DebianRepository/Unofficial)
+  #wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  #$apt install ./google-chrome-stable_current_amd64.deb
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+  echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+  apt update
+  $apt install google-chrome-stable
 
   # Eclipse
   if ! test -e /usr/bin/eclipse ; then
@@ -128,7 +141,25 @@ EOM
   fi
   $apt install default-jre
 
-  $apt install wine
+  # visual studio code from https://code.visualstudio.com/docs/setup/linux
+  if ! test -f /usr/share/keyrings/packages.microsoft.gpg ; then
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /usr/share/keyrings/packages.microsoft.gpg
+  fi
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
+  #$apt install apt-transport-https
+  apt update
+  $apt install code # or code-insiders
+  $apt install gvfs-bin
+  #update-alternatives --set editor /usr/bin/code
+  #echo "fs.inotify.max_user_watches=524288" > /etc/sysctl.d/10-visual-studio-code.conf
+  # Launch VS Code Quick Open (Ctrl+P): ext install ms-vscode.cpptools
+  # Launch VS Code Quick Open (Ctrl+P): ext install ms-python.python
+
+  if ! test -f /var/lib/dpkg/arch ; then
+    dpkg --add-architecture i386
+    apt update
+  fi
+  $apt install wine winetricks wine32
 fi
 # Company dependent apps:
 if false ; then
@@ -139,7 +170,8 @@ fi
 # Generic devel environment:
 $apt install build-essential autoconf libtool libtool-bin pkg-config bison flex git libacl1-dev libssl-dev
 $apt install gawk bc make git-email ccache indent gperf
-#$apt install python perl clang golang
+#$apt install perl clang golang
+#$apt install python pylint pyflakes pyflakes3
 #$apt install subversion git-svn
 #$apt install openjdk-8-jdk cmake
 #$apt install gcc-arm-none-eabi g++-aarch64-linux-gnu g++-arm-linux-gnueabihf
@@ -174,13 +206,13 @@ EOM
     # sudo make install
   fi
 fi
-if test $unstable = 0 -a ! -d /opt/qemu ; then
+if test $unstable = 0 -a $testing = 0 -a ! -d /opt/qemu ; then
   $apt install pkg-config libglib2.0-dev libpixman-1-dev
-  if ! test -f /home/$NEWUSER/data/qemu-4.1.0.tar.xz ; then
-    su $NEWUSER -c "cd ~/data && wget -q https://download.qemu.org/qemu-4.1.0.tar.xz"
+  if ! test -f /home/$NEWUSER/data/qemu-4.2.0.tar.xz ; then
+    su $NEWUSER -c "cd ~/data && wget -q https://download.qemu.org/qemu-4.2.0.tar.xz"
   fi
-  #tar xJf qemu-4.1.0.tar.xz
-  #cd qemu-4.1.0
+  #tar xJf qemu-4.2.0.tar.xz
+  #cd qemu-4.2.0
   #./configure --prefix=/opt/qemu
   #make -j 8
   #sudo make install
@@ -188,4 +220,7 @@ fi
 
 apt clean
 apt update
+
+# If this should again be used as a generic image:
+#dd if=/dev/zero of=/ZERO; rm -f /ZERO # zero unused filesystem
 
