226adcf10c0095feb7e5f3950a3831e9c2e827d5
Florian La Roche <Florian.LaRoche@gmail.com>
2020-04-19 18:11:36 +0200

    kernel56.sh: Debian upstream (Ben) has fixed arm64/armhf builds

--- vmdb2-debian/kernel56.sh
+++ vmdb2-debian/kernel56.sh
@@ -73,12 +73,7 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b master https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-sed -i -e '1 s/5.6.4-1~exp1/5.6.5-1~exp1/' linux-5/debian/changelog
-if test $CROSS = 1 ; then
-sed -i -e 's,^features/all/lockdown,#features/all/lockdown,g' linux-5/debian/patches/series
-sed -i -e 's,^features/all/db-mok-keyring,#features/all/db-mok-keyring,g' linux-5/debian/patches/series
-sed -i -e '/CONFIG_LOCK_DOWN_IN_EFI_SECURE_BOOT/d' linux-5/debian/bin/gencontrol_signed.py
-fi
+sed -i -e '1 s/5.6.4-1~exp2/5.6.5-1~exp2/' linux-5/debian/changelog
 #sed -i -e 's,^features/all/lockdown,#features/all/lockdown,g' linux-5/debian/patches/series
 sed -i -e 's,time-sched_clock-Expire-timer-in-hardirq-context.patch,,g' linux-5/debian/patches-rt/series
 sed -i -e 's,pci-switchtec-Don-t-use-completion-s-wait-queue.patch,,g' linux-5/debian/patches-rt/series
