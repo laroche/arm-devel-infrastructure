e0bac9209f68861e27a34b7c7cb71d7f925423bf
Florian La Roche <Florian.LaRoche@gmail.com>
2020-06-20 08:26:44 +0200

    setup.sh: sid is not testing, but unstable

--- vmdb2-debian/setup.sh
+++ vmdb2-debian/setup.sh
@@ -72,11 +72,11 @@ SYSTYPE="$(systemd-detect-virt)"
 apt="apt-get -qq -y"
 
 unstable="0"
-if grep -q unstable /etc/apt/sources.list ; then
+if grep -q unstable /etc/apt/sources.list || grep -qw sid /etc/apt/sources.list ; then
   unstable="1"
 fi
 testing="0"
-if grep -q testing /etc/apt/sources.list || grep -qw sid /etc/apt/sources.list || grep -q bullseye /etc/apt/sources.list ; then
+if grep -q testing /etc/apt/sources.list || grep -q bullseye /etc/apt/sources.list ; then
   testing="1"
 fi
 
