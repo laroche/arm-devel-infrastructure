c3b6c93746dfc45df5bdb89fe4146ae18a20ecc1
Florian La Roche <Florian.LaRoche@gmail.com>
2020-10-02 12:20:15 +0200

    update to kernel 5.8.13

--- vmdb2-debian/kernel58.sh
+++ vmdb2-debian/kernel58.sh
@@ -49,7 +49,7 @@ if test $CROSS = 1 ; then
 fi
 fi
 
-KVER=5.8.12
+KVER=5.8.13
 
 if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
@@ -75,8 +75,8 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-sed -i -e '1 s/5.8.10-2/5.8.12-1/' linux-5/debian/changelog
-#sed -i -e 's,^bugfix/all/net-packet-fix-overflow-in-tpacket_rcv.patch,,g' linux-5/debian/patches/series
+sed -i -e '1 s/5.8.10-2/5.8.13-1/' linux-5/debian/changelog
+sed -i -e 's,^bugfix/all/netfilter-ctnetlink-add-a-range-check-for-l3-l4-prot.patch,,g' linux-5/debian/patches/series
 #sed -i -e 's,pci-switchtec-Don-t-use-completion-s-wait-queue.patch,,g' linux-5/debian/patches-rt/series
 #exit 0
 test -f orig/linux_$KVER.orig.tar.xz || wget -q https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$KVER.tar.xz
