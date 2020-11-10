e8599d915548d8b6b26fc85e3038aa5b702731fc
Florian La Roche <Florian.LaRoche@gmail.com>
2020-11-10 13:55:10 +0100

    update to kernel 5.9.7

--- vmdb2-debian/kernel59.sh
+++ vmdb2-debian/kernel59.sh
@@ -49,7 +49,7 @@ if test $CROSS = 1 ; then
 fi
 fi
 
-KVER=5.9.6
+KVER=5.9.7
 
 if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
@@ -75,9 +75,9 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-#sed -i -e '1 s/5.9.4-1/5.9.6-1/' linux-5/debian/changelog
-#sed -i -e '1 s/UNRELEASED/unstable/' linux-5/debian/changelog
-#sed -i -e 's,^bugfix/all/firmware_class-log-every-success-and-failure.patch,,g' linux-5/debian/patches/series
+sed -i -e '1 s/5.9.6-1/5.9.7-1/' linux-5/debian/changelog
+sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
+sed -i -e 's,^bugfix/all/perf-core-Fix-a-memory-leak-in-perf_event_parse_addr.patch,,g' linux-5/debian/patches/series
 #sed -i -e 's,pci-switchtec-Don-t-use-completion-s-wait-queue.patch,,g' linux-5/debian/patches-rt/series
 #exit 0
 test -f orig/linux_$KVER.orig.tar.xz || wget -q https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$KVER.tar.xz
