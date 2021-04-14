bd58209437707cbdb514437f5cb533e92ac38ef3
Florian La Roche <Florian.LaRoche@gmail.com>
2021-04-14 10:48:16 +0200

    disable already applied rt patch

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -82,7 +82,7 @@ sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
 sed -i -e 's,^debian/makefile-do-not-check-for-libelf-when-building-oot-module.patch,,g' linux-5/debian/patches/series
 sed -i -e 's,^bugfix/x86/0001-bpf-x86-Validate-computation-of-branch-displacements.patch,,g' linux-5/debian/patches/series
 sed -i -e 's,^bugfix/x86/0002-bpf-x86-Validate-computation-of-branch-displacements.patch,,g' linux-5/debian/patches/series
-#sed -i -e 's,smp-Process-pending-softirqs-in-flush_smp_call_funct.patch,,g' linux-5/debian/patches-rt/series
+sed -i -e 's,0185-xfrm-Use-sequence-counter-with-associated-spinlock.patch,,g' linux-5/debian/patches-rt/series
 sed -i -e 's/CONFIG_DRM_AST=m/#CONFIG_DRM_AST is not set/g' linux-5/debian/config/arm64/config
 sed -i -e 's/^ast//g' linux-5/debian/installer/modules/arm64/fb-modules
 #exit 0
