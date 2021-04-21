88e3bf997d00810b89e41042b0406bfc87893d76
Florian La Roche <Florian.LaRoche@gmail.com>
2021-04-21 15:19:36 +0200

    update to kernel 5.10.32 and 5.11.16

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -49,11 +49,11 @@ if test $CROSS = 1 ; then
 fi
 fi
 
-KVER=5.10.31
+KVER=5.10.32
 
 if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
-  RVER=5.10.27
+  RVER=5.10.31
 fi
 
 if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
@@ -67,7 +67,7 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
     popd
   fi
   cd rpi-linux-5 || exit 1
-  git format-patch -o ../rpi-patches-$RVER 472493c8a425f62200882c2c6acb1be2e29b3c03
+  git format-patch -o ../rpi-patches-$RVER 65f1995ea1e930674e76c5888b4643581e11434c
   cd ..
   #rm -fr rpi-linux-5
 fi
@@ -76,12 +76,13 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-sed -i -e '1 s/5.10.28-2/5.10.31-1/' linux-5/debian/changelog
+sed -i -e '1 s/5.10.28-2/5.10.32-1/' linux-5/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
 sed -i -e 's,^debian/makefile-do-not-check-for-libelf-when-building-oot-module.patch,,g' linux-5/debian/patches/series
 sed -i -e 's,^bugfix/x86/0001-bpf-x86-Validate-computation-of-branch-displacements.patch,,g' linux-5/debian/patches/series
 sed -i -e 's,^bugfix/x86/0002-bpf-x86-Validate-computation-of-branch-displacements.patch,,g' linux-5/debian/patches/series
+sed -i -e 's,^bugfix/all/tools-include-uapi-fix-errno.h.patch,,g' linux-5/debian/patches/series
 sed -i -e 's,0185-xfrm-Use-sequence-counter-with-associated-spinlock.patch,,g' linux-5/debian/patches-rt/series
 sed -i -e 's/CONFIG_DRM_AST=m/#CONFIG_DRM_AST is not set/g' linux-5/debian/config/arm64/config
 sed -i -e 's/^ast//g' linux-5/debian/installer/modules/arm64/fb-modules
