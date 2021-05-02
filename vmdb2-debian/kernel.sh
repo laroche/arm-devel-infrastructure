1b2db77b1ead844ce98d55855854087f2f0acac9
Florian La Roche <Florian.LaRoche@gmail.com>
2021-05-02 18:58:26 +0200

    update to kernel 5.10.34 and 5.11.18

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -49,7 +49,7 @@ if test $CROSS = 1 ; then
 fi
 fi
 
-KVER=5.10.33
+KVER=5.10.34
 
 if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
@@ -76,7 +76,7 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-sed -i -e '1 s/5.10.28-2/5.10.33-1/' linux-5/debian/changelog
+sed -i -e '1 s/5.10.28-2/5.10.34-1/' linux-5/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
 sed -i -e 's,^debian/makefile-do-not-check-for-libelf-when-building-oot-module.patch,,g' linux-5/debian/patches/series
