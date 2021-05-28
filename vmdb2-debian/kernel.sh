646b2ab517ed3a79615b23756fef9e2a09f2e4e9
Florian La Roche <Florian.LaRoche@gmail.com>
2021-05-28 16:55:44 +0200

    update to Linux kernel 5.10.41 and 5.12.8

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -49,7 +49,7 @@ if test $CROSS = 1 ; then
 fi
 fi
 
-KVER=5.10.40
+KVER=5.10.41
 
 if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
@@ -76,10 +76,12 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-sed -i -e '1 s/5.10.38-1/5.10.40-1/' linux-5/debian/changelog
+sed -i -e '1 s/5.10.40-1/5.10.41-1/' linux-5/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
-#sed -i -e 's,^bugfix/all/Revert-net-sctp-fix-race-condition-in-sctp_destroy_s.patch,,g' linux-5/debian/patches/series
+sed -i -e 's,^bugfix/all/bpf-wrap-aux-data-inside-bpf_sanitize_info-container.patch,,g' linux-5/debian/patches/series
+sed -i -e 's,^bugfix/all/bpf-fix-mask-direction-swap-upon-off-reg-sign-change.patch,,g' linux-5/debian/patches/series
+sed -i -e 's,^bugfix/all/bpf-no-need-to-simulate-speculative-domain-for-immediates.patch,,g' linux-5/debian/patches/series
 #sed -i -e 's,0038-powerpc-mm-highmem-Switch-to-generic-kmap-atomic.patch,,g' linux-5/debian/patches-rt/series
 sed -i -e 's/CONFIG_DRM_AST=m/#CONFIG_DRM_AST is not set/g' linux-5/debian/config/arm64/config
 sed -i -e 's/^ast//g' linux-5/debian/installer/modules/arm64/fb-modules
