233eb0784fade13ad6233c937d1428cf5c822347
Florian La Roche <Florian.LaRoche@gmail.com>
2021-06-18 12:00:36 +0200

    update to Linux kernel 5.10.45 and 5.12.12

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -49,7 +49,7 @@ if test $CROSS = 1 ; then
 fi
 fi
 
-KVER=5.10.44
+KVER=5.10.45
 
 if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
@@ -76,7 +76,7 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-sed -i -e '1 s/5.10.43-1/5.10.44-1/' linux-5/debian/changelog
+#sed -i -e '1 s/5.10.44-1/5.10.45-1/' linux-5/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
 #sed -i -e 's,^bugfix/all/net-usb-cdc_ncm-don-t-spew-notifications.patch,,g' linux-5/debian/patches/series
