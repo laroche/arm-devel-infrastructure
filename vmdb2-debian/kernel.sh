184f3de2527530e64bad1960bfeb1bcd80dd15a1
Florian La Roche <Florian.LaRoche@gmail.com>
2021-09-04 22:53:55 +0200

    kernel.sh: arm* changes

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -67,7 +67,7 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
     popd
   fi
   cd rpi-linux-5 || exit 1
-  git format-patch -o ../rpi-patches-$RVER f428e49b8cb1fbd9b4b4b29ea31b6991d2ff7de1
+  git format-patch -o ../rpi-patches-$RVER 7c60610d476766e128cc4284bb6349732cbd6606
   cd ..
   #rm -fr rpi-linux-5
 fi
@@ -76,7 +76,7 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b master https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-sed -i -e '1 s/5.14-1~exp1/5.14.1-1/' linux-5/debian/changelog
+sed -i -e '1 s/5.14-1~exp2/5.14.1-1/' linux-5/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
 #sed -i -e 's,^bugfix/all/Revert-PCI-PM-Do-not-read-power-state-in-pci_enable_.patch,,g' linux-5/debian/patches/series
@@ -99,8 +99,7 @@ if test "$RPIPATCHES" = 1 ; then
   pushd debian/patches
     mkdir bugfix/rpi
     cp ../../../rpi-patches-$RVER/*.patch bugfix/rpi/
-    rm -f bugfix/rpi/0337-net-lan78xx-Ack-pending-PHY-ints-when-resetting.patch \
-          bugfix/rpi/0339-drm-atomic-Pass-the-full-state-to-CRTC-atomic-enable.patch
+    rm -f bugfix/rpi/0319-drm-atomic-Pass-the-full-state-to-CRTC-atomic-enable.patch
     ls bugfix/rpi/*.patch >> series
   popd
   rm -f debian/abi/5.14.0-?/arm*
