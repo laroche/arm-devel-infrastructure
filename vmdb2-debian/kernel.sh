9fec70d834a185df2220f87290e6e62d462c2229
Florian La Roche <Florian.LaRoche@gmail.com>
2021-11-06 15:03:51 +0100

    update to Linux kernel 5.15.1

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -49,7 +49,7 @@ if test $CROSS = 1 ; then
 fi
 fi
 
-KVER=5.15
+KVER=5.15.1
 
 if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
@@ -76,10 +76,12 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b master https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-sed -i -e '1 s/5.15~rc7-1~exp1/5.15-1/' linux-5/debian/changelog
+sed -i -e '1 s/5.15-/5.15.1-/' linux-5/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
-sed -i -e 's,^bugfix/arm/ARM-dts-sun7i-A20-olinuxino-lime2-Fix-ethernet-phy-m.patch,,g' linux-5/debian/patches/series
+#sed -i -e 's,^bugfix/arm/ARM-dts-sun7i-A20-olinuxino-lime2-Fix-ethernet-phy-m.patch,,g' linux-5/debian/patches/series
+sed -i -e 's,^bugfix/all/sfc-Fix-reading-non-legacy-supported-link-modes.patch,,g' linux-5/debian/patches/series
+sed -i -e 's,^bugfix/all/media-firewire-firedtv-avc-fix-a-buffer-overflow-in-.patch,,g' linux-5/debian/patches/series
 #sed -i -e 's,0038-powerpc-mm-highmem-Switch-to-generic-kmap-atomic.patch,,g' linux-5/debian/patches-rt/series
 sed -i -e 's/CONFIG_DRM_AST=m/#CONFIG_DRM_AST is not set/g' linux-5/debian/config/arm64/config
 sed -i -e 's/^ast//g' linux-5/debian/installer/modules/arm64/fb-modules
