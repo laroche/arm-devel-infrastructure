42ff76bb3378767878b036ac0cabd7a1bd9b29b2
Florian La Roche <Florian.LaRoche@gmail.com>
2021-11-07 10:01:30 +0100

    kernel.sh: cleanup

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -76,11 +76,10 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b master https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-sed -i -e '1 s/5.15-/5.15.1-/' linux-5/debian/changelog
+#sed -i -e '1 s/5.15-/5.15.1-/' linux-5/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
-sed -i -e 's,^bugfix/all/sfc-Fix-reading-non-legacy-supported-link-modes.patch,,g' linux-5/debian/patches/series
-sed -i -e 's,^bugfix/all/media-firewire-firedtv-avc-fix-a-buffer-overflow-in-.patch,,g' linux-5/debian/patches/series
+#sed -i -e 's,^bugfix/all/sfc-Fix-reading-non-legacy-supported-link-modes.patch,,g' linux-5/debian/patches/series
 #sed -i -e 's,0038-powerpc-mm-highmem-Switch-to-generic-kmap-atomic.patch,,g' linux-5/debian/patches-rt/series
 sed -i -e 's/CONFIG_DRM_AST=m/#CONFIG_DRM_AST is not set/g' linux-5/debian/config/arm64/config
 sed -i -e 's/^ast//g' linux-5/debian/installer/modules/arm64/fb-modules
