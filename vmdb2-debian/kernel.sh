eee599ca707a30d1e1f115a04336a55a22455485
Florian La Roche <Florian.LaRoche@gmail.com>
2021-11-21 18:06:03 +0100

    update to Linux kernel 5.15.4

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -49,7 +49,7 @@ if test $CROSS = 1 ; then
 fi
 fi
 
-KVER=5.15.3
+KVER=5.15.4
 
 if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
@@ -76,7 +76,7 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-#sed -i -e '1 s/5.15.2-/5.15.3-/' linux-5/debian/changelog
+sed -i -e '1 s/5.15.3-/5.15.4-/' linux-5/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
 #sed -i -e 's,^bugfix/all/sfc-Fix-reading-non-legacy-supported-link-modes.patch,,g' linux-5/debian/patches/series
