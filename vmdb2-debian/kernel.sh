4c5421a4aa252921af89b0f64ee4112f5160d8ac
Florian La Roche <Florian.LaRoche@gmail.com>
2021-12-01 12:17:17 +0100

    update to Linux kernel 5.15.6

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -49,7 +49,7 @@ if test $CROSS = 1 ; then
 fi
 fi
 
-KVER=5.15.5
+KVER=5.15.6
 
 if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
@@ -76,10 +76,10 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-#sed -i -e '1 s/5.15.4-/5.15.5-/' linux-5/debian/changelog
+sed -i -e '1 s/5.15.5-/5.15.6-/' linux-5/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
-#sed -i -e 's,^features/arm64/arm64-dts-rockchip-disable-USB-type-c-DisplayPort.patch,,g' linux-5/debian/patches/series
+sed -i -e 's,^bugfix/all/fuse-release-pipe-buf-after-last-use.patch,,g' linux-5/debian/patches/series
 #sed -i -e 's,0038-powerpc-mm-highmem-Switch-to-generic-kmap-atomic.patch,,g' linux-5/debian/patches-rt/series
 sed -i -e 's/CONFIG_DRM_AST=m/#CONFIG_DRM_AST is not set/g' linux-5/debian/config/arm64/config
 sed -i -e 's/^ast//g' linux-5/debian/installer/modules/arm64/fb-modules
@@ -99,7 +99,8 @@ if test "$RPIPATCHES" = 1 ; then
   pushd debian/patches
     mkdir bugfix/rpi
     cp ../../../rpi-patches-$RVER/*.patch bugfix/rpi/
-    #rm -f bugfix/rpi/0318-drm-atomic-Pass-the-full-state-to-CRTC-atomic-enable.patch
+    rm -f bugfix/rpi/0632-media-v4l2-core-fix-VIDIOC_DQEVENT-handling-on-non-x.patch \
+	  bugfix/rpi/0638-ARM-dts-bcm2711-Fix-PCIe-interrupts.patch
     ls bugfix/rpi/*.patch >> series
   popd
   echo "CONFIG_PCIE_BRCMSTB=y" >> debian/config/config
