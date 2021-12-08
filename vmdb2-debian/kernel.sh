411a7a4f1ab2d14dc2e3b222f03e8144f857ba1b
Florian La Roche <Florian.LaRoche@gmail.com>
2021-12-08 10:57:02 +0100

    update to Linux kernel 5.15.7

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -49,11 +49,11 @@ if test $CROSS = 1 ; then
 fi
 fi
 
-KVER=5.15.6
+KVER=5.15.7
 
 if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
-  RVER=5.15.5
+  RVER=5.15.6
 fi
 
 if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
@@ -67,7 +67,7 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
     popd
   fi
   cd rpi-linux-5 || exit 1
-  git format-patch -o ../rpi-patches-$RVER f00712e27083550be3031099b7697925533a6e01
+  git format-patch -o ../rpi-patches-$RVER a2547651bc896f95a3680a6a0a27401e7c7a1080
   cd ..
   #rm -fr rpi-linux-5
 fi
@@ -76,7 +76,7 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-sed -i -e '1 s/5.15.5-/5.15.6-/' linux-5/debian/changelog
+sed -i -e '1 s/5.15.5-/5.15.7-/' linux-5/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
 sed -i -e 's,^bugfix/all/fuse-release-pipe-buf-after-last-use.patch,,g' linux-5/debian/patches/series
@@ -99,8 +99,8 @@ if test "$RPIPATCHES" = 1 ; then
   pushd debian/patches
     mkdir bugfix/rpi
     cp ../../../rpi-patches-$RVER/*.patch bugfix/rpi/
-    rm -f bugfix/rpi/0632-media-v4l2-core-fix-VIDIOC_DQEVENT-handling-on-non-x.patch \
-	  bugfix/rpi/0638-ARM-dts-bcm2711-Fix-PCIe-interrupts.patch
+    #rm -f bugfix/rpi/0632-media-v4l2-core-fix-VIDIOC_DQEVENT-handling-on-non-x.patch \
+    #      bugfix/rpi/0638-ARM-dts-bcm2711-Fix-PCIe-interrupts.patch
     ls bugfix/rpi/*.patch >> series
   popd
   echo "CONFIG_PCIE_BRCMSTB=y" >> debian/config/config
