ad21d441cbe894b479ac5166c9b355567f3f85a8
Florian La Roche <Florian.LaRoche@gmail.com>
2022-01-21 08:45:05 +0100

    update to Linux kernel 5.16.2 and 5.15.16

--- vmdb2-debian/kernel-5.16.sh
+++ vmdb2-debian/kernel-5.16.sh
@@ -49,8 +49,8 @@ if test $CROSS = 1 ; then
 fi
 fi
 
-KVER=5.16.1
-#KVERR=5.16.1
+KVER=5.16.2
+#KVERR=5.16.2
 
 if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
@@ -77,7 +77,7 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b master https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-#sed -i -e '1 s/5.16.1/5.16.2/' linux-5/debian/changelog
+sed -i -e '1 s/5.16.1/5.16.2/' linux-5/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
 #sed -i -e 's,^bugfix/all/bpf-fix-kernel-address-leakage-in-atomic-fetch.patch,,g' linux-5/debian/patches/series
--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -49,7 +49,7 @@ if test $CROSS = 1 ; then
 fi
 fi
 
-KVER=5.15.15
+KVER=5.15.16
 
 if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
@@ -76,10 +76,10 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-#sed -i -e '1 s/5.15.14-/5.15.15-/' linux-5/debian/changelog
+sed -i -e '1 s/5.15.15-/5.15.16-/' linux-5/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
-#sed -i -e 's,^bugfix/all/bpf-fix-out-of-bounds-access-from-invalid-_or_null-type-verification.patch,,g' linux-5/debian/patches/series
+sed -i -e 's,^bugfix/all/vfs-fs_context-fix-up-param-length-parsing-in-legacy.patch,,g' linux-5/debian/patches/series
 #sed -i -e 's,0038-powerpc-mm-highmem-Switch-to-generic-kmap-atomic.patch,,g' linux-5/debian/patches-rt/series
 sed -i -e 's/CONFIG_DRM_AST=m/#CONFIG_DRM_AST is not set/g' linux-5/debian/config/arm64/config
 sed -i -e 's/^ast//g' linux-5/debian/installer/modules/arm64/fb-modules
