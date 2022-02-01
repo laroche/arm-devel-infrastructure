9a36cf0941ab67f6494618430125be88be18a5bb
Florian La Roche <Florian.LaRoche@gmail.com>
2022-02-01 22:25:17 +0100

    update to Linux kernel 5.16.5

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -49,7 +49,7 @@ if test $CROSS = 1 ; then
 fi
 fi
 
-KVER=5.15.18
+KVER=5.15.19
 
 if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
@@ -76,7 +76,7 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git linux-5
 fi
 # Change Debian source to new version:
-sed -i -e '1 s/5.15.15-/5.15.18-/' linux-5/debian/changelog
+sed -i -e '1 s/5.15.15-/5.15.19-/' linux-5/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
 sed -i -e 's,^bugfix/all/vfs-fs_context-fix-up-param-length-parsing-in-legacy.patch,,g' linux-5/debian/patches/series
