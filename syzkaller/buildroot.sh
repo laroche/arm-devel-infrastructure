558e9dffd93be64cc5087e117ff932e827d383d1
Max Mustermann <Max.Mustermann@example.org>
2022-05-12 21:32:55 +0200

    complete aarch64 info

--- syzkaller/buildroot.sh
+++ syzkaller/buildroot.sh
@@ -19,7 +19,9 @@ test -d buildroot && {
 
 # make qemu_arm_vexpress_defconfig
 # make
-# qemu-system-arm -M vexpress-a9 -cpu cortex-a9 -smp 4 -m 256 -kernel output/images/zImage -dtb output/images/vexpress-v2p-ca9.dtb -drive file=output/images/rootfs.ext2,if=sd,format=raw -append "console=ttyAMA0,115200 root=/dev/mmcblk0" -serial stdio -net nic,model=lan9118 -net user -display none
+# qemu-system-arm -M vexpress-a9 -cpu cortex-a9 -smp 4 -m 256 -kernel output/images/zImage -dtb output/images/vexpress-v2p-ca9.dtb -drive file=output/images/rootfs.ext2,if=sd,format=raw -append "console=ttyAMA0,115200 rootwait root=/dev/mmcblk0" -net nic,model=lan9118 -net user -nographic # -serial stdio -display none
 
+# make qemu_aarch64_virt_defconfig
+# make
 # qemu-system-aarch64 -M virt -cpu cortex-a53 -smp 4 -m 768 -kernel output/images/Image -append "rootwait console=ttyAMA0 root=/dev/vda" -netdev user,id=eth0 -device virtio-net-device,netdev=eth0 -drive file=output/images/rootfs.ext4,if=none,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -serial stdio -display none
 
