2b2441eed23bebad6a78f6fddf4f99b349a496f9
Max Mustermann <Max.Mustermann@example.org>
2022-05-13 07:57:30 +0200

    buildroot.sh: adjust call to qemu for arm64

--- syzkaller/buildroot.sh
+++ syzkaller/buildroot.sh
@@ -23,5 +23,5 @@ test -d buildroot && {
 
 # make qemu_aarch64_virt_defconfig
 # make
-# qemu-system-aarch64 -M virt -cpu cortex-a53 -smp 4 -m 768 -kernel output/images/Image -append "rootwait console=ttyAMA0 root=/dev/vda" -netdev user,id=eth0 -device virtio-net-device,netdev=eth0 -drive file=output/images/rootfs.ext4,if=none,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -serial stdio -display none
+# qemu-system-aarch64 -M virt -cpu cortex-a53 -smp 4 -m 768 -kernel output/images/Image -append "rootwait root=/dev/vda console=ttyAMA0" -netdev user,id=eth0 -device virtio-net-device,netdev=eth0 -drive file=output/images/rootfs.ext4,if=none,format=raw,id=hd0 -device virtio-blk-device,drive=hd0 -nographic # -serial stdio -display none
 
