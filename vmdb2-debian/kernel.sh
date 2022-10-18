e4617225554cb578944c74af74083ceaebd8d056
Florian La Roche <Florian.LaRoche@gmail.com>
2022-10-18 14:53:21 +0200

    update to Linux kernel 6.0.2

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -19,10 +19,10 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=5.19.16
-KVERR=5.19.16
+KVER=6.0.2
+KVERR=6.0.2
 CDIR=linux-$KVERR
-RVER=5.19.11
+RVER=6.0.2
 
 CROSS=0
 ARCH=
@@ -60,25 +60,25 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
   # Extract the raspberry-pi patches into a subdirectory:
   RDIR=rpi-linux-$RVER
   if test ! -d $RDIR ; then
-    git clone -b rpi-5.19.y https://github.com/raspberrypi/linux/ $RDIR
+    git clone -b rpi-6.0.y https://github.com/raspberrypi/linux/ $RDIR
     test -d $RDIR || exit 1
   else
     pushd $RDIR
-    git checkout rpi-5.19.y
+    git checkout rpi-6.0.y
     popd
   fi
   cd $RDIR || exit 1
-  git format-patch -o ../rpi-patches-$RVER fcf22aefe87101424563a8dd8adec8a75b8c7576
+  git format-patch -o ../rpi-patches-$RVER dab08f7eecdfa2ea024dc563dc09afae137e3b38
   cd ..
   rm -fr $RDIR
 fi
 
 if ! test -d $CDIR ; then
-  git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git $CDIR
+  git clone --single-branch --depth 1 -b master https://salsa.debian.org/kernel-team/linux.git $CDIR
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/5.19.11-/5.19.16-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.0.2-/6.0.2-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^debian/export-symbols-needed-by-android-drivers.patch,,g' $CDIR/debian/patches/series
@@ -107,9 +107,9 @@ if test "$RPIPATCHES" = 1 ; then
   echo "CONFIG_RESET_RASPBERRY=y" >> debian/config/config
   echo "CONFIG_RESET_BRCMSTB_RESCAL=y" >> debian/config/config
   echo "CONFIG_NO_HZ_FULL=y" >> debian/config/featureset-rt/config
-  rm -f debian/abi/5.19.0-*/arm*
+  rm -f debian/abi/6.0.0-*/arm*
 fi
-rm -fr debian/abi/5.19.0-*
+rm -fr debian/abi/6.0.0-*
 
 if test $CROSS = 0 ; then
 
