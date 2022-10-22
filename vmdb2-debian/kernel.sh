9a33125f77d86e4d6382cb7700f9f0a82caba288
Florian La Roche <Florian.LaRoche@gmail.com>
2022-10-22 11:41:25 +0200

    update to Linux kernel 6.0.3

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -19,10 +19,10 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.0.2
-KVERR=6.0.2
+KVER=6.0.3
+KVERR=6.0.3
 CDIR=linux-$KVERR
-RVER=6.0.2
+RVER=6.0.3
 
 CROSS=0
 ARCH=
@@ -68,17 +68,17 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
     popd
   fi
   cd $RDIR || exit 1
-  git format-patch -o ../rpi-patches-$RVER dab08f7eecdfa2ea024dc563dc09afae137e3b38
+  git format-patch -o ../rpi-patches-$RVER e6f4ff3f91251f67b130c29f38673eb5702f88b9
   cd ..
   rm -fr $RDIR
 fi
 
 if ! test -d $CDIR ; then
-  git clone --single-branch --depth 1 -b master https://salsa.debian.org/kernel-team/linux.git $CDIR
+  git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git $CDIR
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.0.2-/6.0.2-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.0.2-/6.0.3-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^debian/export-symbols-needed-by-android-drivers.patch,,g' $CDIR/debian/patches/series
