f643172ac66644a25a6959ac5cd01ff8bc06d8e9
Florian La Roche <Florian.LaRoche@gmail.com>
2022-10-30 11:19:33 +0100

    update to Linux kernel 5.10.152

--- vmdb2-debian/kernel-5.10.sh
+++ vmdb2-debian/kernel-5.10.sh
@@ -49,7 +49,7 @@ if test $CROSS = 1 ; then
 fi
 fi
 
-KVER=5.10.151
+KVER=5.10.152
 
 if test $RPIPATCHES = 1 ; then
   #RVER=$KVER
@@ -77,13 +77,11 @@ if ! test -d linux-5 ; then
   git clone --single-branch --depth 1 -b 5.10-stable-updates https://salsa.debian.org/carnil/linux.git linux-5
 fi
 # Change Debian source to new version:
-sed -i -e '1 s/5.10.148/5.10.151/' linux-5/debian/changelog
+sed -i -e '1 s/5.10.151/5.10.152/' linux-5/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
 sed -i -e '1 s/bullseye/UNRELEASED/' linux-5/debian/changelog
-sed -i -e 's,^bugfix/all/mac80211-mlme-find-auth-challenge-directly.patch,,g' linux-5/debian/patches/series
-sed -i -e 's,^bugfix/all/wifi-mac80211-don-t-parse-mbssid-in-assoc-response.patch,,g' linux-5/debian/patches/series
-sed -i -e 's,^bugfix/all/wifi-mac80211-fix-MBSSID-parsing-use-after-free.patch,,g' linux-5/debian/patches/series
+#sed -i -e 's,^bugfix/all/wifi-mac80211-fix-MBSSID-parsing-use-after-free.patch,,g' linux-5/debian/patches/series
 #sed -i -e 's,0114-tcp-Remove-superfluous-BH-disable-around-listening_h.patch,,g' linux-5/debian/patches-rt/series
 sed -i -e 's/CONFIG_DRM_AST=m/#CONFIG_DRM_AST is not set/g' linux-5/debian/config/arm64/config
 sed -i -e 's/^ast//g' linux-5/debian/installer/modules/arm64/fb-modules
