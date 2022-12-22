7ab003190862e2733eff7db254839437b575d731
Florian La Roche <Florian.LaRoche@gmail.com>
2022-12-22 07:44:48 +0100

    update to Linux kernel 6.0.15

--- vmdb2-debian/kernel-6.0.sh
+++ vmdb2-debian/kernel-6.0.sh
@@ -19,8 +19,8 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.0.14
-KVERR=6.0.14
+KVER=6.0.15
+KVERR=6.0.15
 CDIR=linux-$KVERR
 RVER=6.0.13
 
@@ -78,7 +78,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.0.12-/6.0.14-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.0.12-/6.0.15-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e 's,^bugfix/all/xen-netback-Ensure-protocol-headers-don-t-fall-in-th.patch,,g' $CDIR/debian/patches/series
