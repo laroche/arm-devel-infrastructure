15bc4c4b127927798f85b09d6f94cbcf88195b8d
Florian La Roche <Florian.LaRoche@gmail.com>
2023-01-27 10:34:25 +0100

    update to Linux kernel 6.1.8

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -19,10 +19,10 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.1.7
-KVERR=6.1.7
+KVER=6.1.8
+KVERR=6.1.8
 CDIR=linux-$KVERR
-RVER=6.1.7
+RVER=6.1.8
 
 CROSS=0
 ARCH=
@@ -68,7 +68,7 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
     popd
   fi
   cd $RDIR || exit 1
-  git format-patch -o ../rpi-patches-$RVER 21e996306a6afaae88295858de0ffb8955173a15
+  git format-patch -o ../rpi-patches-$RVER 93f875a8526a291005e7f38478079526c843cbec
   cd ..
   rm -fr $RDIR
 fi
@@ -78,7 +78,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.1.5-/6.1.6-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.1.7-/6.1.8-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^features/all/security-perf-allow-further-restriction-of-perf_event_open.patch,,g' $CDIR/debian/patches/series
