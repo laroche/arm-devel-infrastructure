ba028ebfc3a11ed62712890dc9b7e4d562fff0b3
Florian La Roche <Florian.LaRoche@gmail.com>
2023-02-06 18:37:43 +0100

    update to Linux kernel 6.1.10

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -19,8 +19,8 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.1.9
-KVERR=6.1.9
+KVER=6.1.10
+KVERR=6.1.10
 CDIR=linux-$KVERR
 RVER=6.1.9
 
@@ -78,7 +78,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.1.8-/6.1.9-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.1.9-/6.1.10-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^features/all/security-perf-allow-further-restriction-of-perf_event_open.patch,,g' $CDIR/debian/patches/series
