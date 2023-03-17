2e7b3820d3c6633f7d2ad44d4819224d100c24b2
Florian La Roche <Florian.LaRoche@gmail.com>
2023-03-17 11:17:00 +0100

    update to Linux kernel 6.1.20

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -19,8 +19,8 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.1.19
-KVERR=6.1.19
+KVER=6.1.20
+KVERR=6.1.20
 CDIR=linux-$KVERR
 RVER=6.1.19
 
@@ -78,7 +78,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.1.18-/6.1.19-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.1.19-/6.1.20-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^bugfix/all/scsi-mpt3sas-Remove-usage-of-dma_get_required_mask-A.patch,,g' $CDIR/debian/patches/series
