df3f8a08af1c638f0b6375a5646eaf320cdb1fbc
Florian La Roche <Florian.LaRoche@gmail.com>
2023-04-26 19:15:03 +0200

    update to Linux kernel 6.1.26

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -19,10 +19,10 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.1.25
-KVERR=6.1.25
+KVER=6.1.26
+KVERR=6.1.26
 CDIR=linux-$KVERR
-RVER=6.1.24
+RVER=6.1.25
 
 CROSS=0
 ARCH=
@@ -68,7 +68,7 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
     popd
   fi
   cd $RDIR || exit 1
-  git format-patch -o ../rpi-patches-$RVER 0102425ac76bd184704c698cab7cb4fe37997556
+  git format-patch -o ../rpi-patches-$RVER f17b0ab65d17988d5e6d6fe22f708ef3721080bf
   cd ..
   rm -fr $RDIR
 fi
@@ -78,7 +78,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.1.24-/6.1.25-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.1.25-/6.1.26-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^bugfix/all/Bluetooth-HCI-Fix-global-out-of-bounds.patch,,g' $CDIR/debian/patches/series
