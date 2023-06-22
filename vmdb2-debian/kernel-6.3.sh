2f12e873064411580b9f7b5528d46bff1df404ae
Florian La Roche <Florian.LaRoche@gmail.com>
2023-06-22 06:35:56 +0200

    update to Linux kernel 6.1.35 and 6.3.9

--- vmdb2-debian/kernel-6.3.sh
+++ vmdb2-debian/kernel-6.3.sh
@@ -19,10 +19,10 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.3.8
-KVERR=6.3.8
+KVER=6.3.9
+KVERR=6.3.9
 CDIR=linux-$KVERR
-RVER=6.3.7
+RVER=6.3.8
 
 CROSS=0
 ARCH=
@@ -68,7 +68,7 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
     popd
   fi
   cd $RDIR || exit 1
-  git format-patch -o ../rpi-patches-$RVER e282393f9d0cd66cee8c68a80f4936f46c449b2d
+  git format-patch -o ../rpi-patches-$RVER f2427f9a3730e9a1a11b69f6b767f7f2fad87523
   cd ..
   rm -fr $RDIR
 fi
@@ -78,7 +78,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.3.7-/6.3.8-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.3.8-/6.3.9-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e 's,^bugfix/all/netfilter-nf_tables-deactivate-anonymous-set-from-pr.patch,,g' $CDIR/debian/patches/series
