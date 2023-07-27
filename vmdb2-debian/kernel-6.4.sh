7d4e4f2873a628d68bc323a64dc94c726273c73c
Florian La Roche <Florian.LaRoche@gmail.com>
2023-07-27 11:47:32 +0200

    update to Linux kernel 6.1.42 and 6.4.7

--- vmdb2-debian/kernel-6.4.sh
+++ vmdb2-debian/kernel-6.4.sh
@@ -19,8 +19,8 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.4.6
-KVERR=6.4.6
+KVER=6.4.7
+KVERR=6.4.7
 CDIR=linux-$KVERR
 RVER=6.4.5
 
@@ -78,7 +78,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.4.4-/6.4.6-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.4.4-/6.4.7-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^bugfix/all/Revert-drm-amd-display-edp-do-not-add-non-edid-timin.patch,,g' $CDIR/debian/patches/series
--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -19,10 +19,10 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.1.41
-KVERR=6.1.41
+KVER=6.1.42
+KVERR=6.1.42
 CDIR=linux-$KVERR
-RVER=6.1.39
+RVER=6.1.41
 
 CROSS=0
 ARCH=
@@ -68,7 +68,7 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
     popd
   fi
   cd $RDIR || exit 1
-  git format-patch -o ../rpi-patches-$RVER a456e17438819ed77f63d16926f96101ca215f09
+  git format-patch -o ../rpi-patches-$RVER 5302e81aa2091d7dd59803f0727d1ba35ed2f929
   cd ..
   rm -fr $RDIR
 fi
@@ -78,7 +78,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.1.38-/6.1.41-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.1.38-/6.1.42-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e 's,^bugfix/all/Revert-drm-amd-display-edp-do-not-add-non-edid-timin.patch,,g' $CDIR/debian/patches/series
