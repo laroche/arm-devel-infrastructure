08254bbc6cc9e10897d34aa2fad002b352d4ba79
Florian La Roche <Florian.LaRoche@gmail.com>
2023-09-02 10:35:23 +0200

    update to Linux kernel 6.1.51 and 6.4.14

--- vmdb2-debian/kernel-6.4.sh
+++ vmdb2-debian/kernel-6.4.sh
@@ -19,8 +19,8 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.4.13
-KVERR=6.4.13
+KVER=6.4.14
+KVERR=6.4.14
 CDIR=linux-$KVERR
 RVER=6.4.11
 
@@ -78,7 +78,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.4.13-/6.4.13-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.4.13-/6.4.14-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^bugfix/x86/x86-retpoline-Don-t-clobber-RFLAGS-during-srso_safe_.patch,,g' $CDIR/debian/patches/series
--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -19,8 +19,8 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.1.50
-KVERR=6.1.50
+KVER=6.1.51
+KVERR=6.1.51
 CDIR=linux-$KVERR
 RVER=6.1.47
 
@@ -78,7 +78,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.1.38-/6.1.50-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.1.38-/6.1.51-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e 's,^bugfix/all/Revert-drm-amd-display-edp-do-not-add-non-edid-timin.patch,,g' $CDIR/debian/patches/series
