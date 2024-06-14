ba62a7889c19b26bdd4d0896e6bc87b89dc56013
Florian La Roche <Florian.LaRoche@gmail.com>
2024-06-14 18:02:27 +0200

    update to Linux kernel 6.9.4
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- vmdb2-debian/kernel-6.9.sh
+++ vmdb2-debian/kernel-6.9.sh
@@ -19,6 +19,11 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
+KVER=6.9.4
+KVERR=6.9.4
+CDIR=linux-$KVERR
+RVER=6.9.4
+
 CROSS=0
 ARCH=
 if test "X$1" = "Xarm64" -o "X$1" = "Xrpi-arm64" ; then
@@ -27,6 +32,7 @@ if test "X$1" = "Xarm64" -o "X$1" = "Xrpi-arm64" ; then
   if test "X$1" = "Xrpi-arm64" ; then
     RPIPATCHES=1
   fi
+  CDIR=$CDIR-arm64
 fi
 if test "X$1" = "Xarmhf" -o "X$1" = "Xrpi-armhf" ; then
   CROSS=1
@@ -34,65 +40,59 @@ if test "X$1" = "Xarmhf" -o "X$1" = "Xrpi-armhf" ; then
   if test "X$1" = "Xrpi-armhf" ; then
     RPIPATCHES=1
   fi
+  CDIR=$CDIR-armhf
 fi
 
 #export LANG=en_US.UTF-8
 
 # Build requirements:
 if true ; then
-sudo apt-get -qq -y install build-essential fakeroot rsync git python3-debian libcap-dev g++-10
+sudo apt-get -qq -y install build-essential fakeroot rsync git python3-debian libcap-dev g++-13
 sudo apt-get -qq -y build-dep linux
 if test $CROSS = 1 ; then
-  sudo apt-get -qq -y install kernel-wedge quilt ccache flex bison libssl-dev
+  sudo apt-get -qq -y install kernel-wedge quilt flex bison libssl-dev ccache
   sudo apt-get -qq -y install crossbuild-essential-arm64 crossbuild-essential-armhf
-  sudo apt-get -qq -y install g++-10-aarch64-linux-gnu g++-10-arm-linux-gnueabihf
+  sudo apt-get -qq -y install g++-13-aarch64-linux-gnu g++-13-arm-linux-gnueabihf
 fi
-fi
-
-KVER=5.10.218
-
-if test $RPIPATCHES = 1 ; then
-  #RVER=$KVER
-  RVER=5.10.95
 fi
 
 if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
   # Extract the raspberry-pi patches into a subdirectory:
-  if test ! -d rpi-linux-5 ; then
-    git clone -b rpi-5.10.y https://github.com/raspberrypi/linux/ rpi-linux-5
-    test -d rpi-linux-5 || exit 1
+  RDIR=rpi-linux-$RVER
+  if test ! -d $RDIR ; then
+    git clone -b rpi-6.9.y https://github.com/raspberrypi/linux/ $RDIR
+    test -d $RDIR || exit 1
   else
-    pushd rpi-linux-5
-    git checkout rpi-5.10.y
+    pushd $RDIR
+    git checkout rpi-6.9.y
     popd
   fi
-  cd rpi-linux-5 || exit 1
-  git format-patch -o ../rpi-patches-$RVER 77656fde3c0125d6ef6f7fb46af6d2739d7b7141
+  cd $RDIR || exit 1
+  git format-patch -o ../rpi-patches-$RVER 103a4e4a4351d3d5214c4f54fdf89f0f81b692ef
   cd ..
-  #rm -fr rpi-linux-5
+  rm -fr $RDIR
 fi
 
-if ! test -d linux-5 ; then
-  #git clone --single-branch --depth 1 -b bullseye https://salsa.debian.org/kernel-team/linux.git linux-5
-  git clone --single-branch --depth 1 -b 5.10-stable-updates https://salsa.debian.org/carnil/linux.git linux-5
+if ! test -d $CDIR ; then
+  #git clone --single-branch --depth 1 -b sid https://salsa.debian.org/kernel-team/linux.git $CDIR
+  #git clone --single-branch --depth 1 -b 6.8-stable-update https://salsa.debian.org/carnil/linux.git $CDIR
+  git clone --single-branch --depth 1 -b update-to-6.9.x https://salsa.debian.org/diederik/linux.git $CDIR
 fi
+sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/5.10.218/5.10.218/' linux-5/debian/changelog
-sed -i -e '1 s/unstable/UNRELEASED/' linux-5/debian/changelog
-sed -i -e '1 s/experimental/UNRELEASED/' linux-5/debian/changelog
-sed -i -e '1 s/bullseye/UNRELEASED/' linux-5/debian/changelog
-sed -i -e 's,^bugfix/all/tipc-fix-UAF-in-error-path.patch,,g' linux-5/debian/patches/series
-sed -i -e 's,^bugfix/all/tipc-fix-a-possible-memleak-in-tipc_buf_append.patch,,g' linux-5/debian/patches/series
-#sed -i -e 's,0128-sched-vtime-Consolidate-IRQ-time-accounting.patch,,g' linux-5/debian/patches-rt/series
-sed -i -e 's/CONFIG_DRM_AST=m/#CONFIG_DRM_AST is not set/g' linux-5/debian/config/arm64/config
-sed -i -e 's/^ast//g' linux-5/debian/installer/modules/arm64/fb-modules
+sed -i -e '1 s/6.9.3-/6.9.4-/' $CDIR/debian/changelog
+sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
+sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
+#sed -i -e 's,^bugfix/all/tipc-fix-UAF-in-error-path.patch,,g' $CDIR/debian/patches/series
+#sed -i -e 's,powerpc-imc-pmu-Use-the-correct-spinlock-initializer.patch,,g' $CDIR/debian/patches-rt/series
 #exit 0
-test -f orig/linux_$KVER.orig.tar.xz || wget -q https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$KVER.tar.xz
-cd linux-5 || exit 1
-test -f ../orig/linux_$KVER.orig.tar.xz || XZ_DEFAULTS="-T 0" debian/bin/genorig.py ../linux-$KVER.tar.xz
+mkdir -p orig
+cd $CDIR || exit 1
+test -f ../orig/linux_$KVER.orig.tar.xz || XZ_DEFAULTS="-T 0" debian/bin/genorig.py https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
 # Just to safe disk space and have a faster compile:
 export DEBIAN_KERNEL_DISABLE_DEBUG=yes
 sed -i -e 's/^debug-info: true/debug-info: false/g' debian/config/defines
+sed -i -e 's/^CONFIG_DEBUG_INFO=y/# CONFIG_DEBUG_INFO is not set/g' debian/config/config
 # Disable RT kernel:
 #if test $CROSS = 1 ; then
 #  sed -i -e 's/^enabled: true/enabled: false/g' debian/config/defines
@@ -102,21 +102,23 @@ if test "$RPIPATCHES" = 1 ; then
   pushd debian/patches
     mkdir bugfix/rpi
     cp ../../../rpi-patches-$RVER/*.patch bugfix/rpi/
-    #rm -f bugfix/rpi/0236-debugobjects-Make-RT-aware.patch
+    rm -f bugfix/rpi/0434-cfg80211-ship-debian-certificates-as-hex-files.patch
+    rm -f bugfix/rpi/0663-module-Avoid-ABI-changes-when-debug-info-is-disabled.patch
     ls bugfix/rpi/*.patch >> series
   popd
   echo "CONFIG_PCIE_BRCMSTB=y" >> debian/config/config
   echo "CONFIG_RESET_RASPBERRY=y" >> debian/config/config
   echo "CONFIG_RESET_BRCMSTB_RESCAL=y" >> debian/config/config
   echo "CONFIG_NO_HZ_FULL=y" >> debian/config/featureset-rt/config
-  rm -f debian/abi/5.10.0-*/arm*
+  rm -f debian/abi/6.9.0-*/arm*
 fi
-rm -fr debian/abi/5.10.0-*
+rm -fr debian/abi/6.9.0-*
 
 if test $CROSS = 0 ; then
 
 debian/rules orig
 debian/rules debian/control
+#debian/rules source
 PAR="$(grep -c ^processor /proc/cpuinfo)"
 #PAR=10
 DEB_BUILD_OPTIONS="parallel=$PAR" XZ_DEFAULTS="-T 0" fakeroot debian/rules binary-arch 2>&1 | tee LOG
@@ -133,6 +135,7 @@ export MAKEFLAGS="-j$(($(nproc)*2))"
 export DEBIAN_KERNEL_DISABLE_DEBUG=
 [ "$(dpkg-parsechangelog --show-field Distribution)" = "UNRELEASED" ] &&
   export DEBIAN_KERNEL_DISABLE_DEBUG=yes
+#export DEBUG_INFO=n
 
 fakeroot make -f debian/rules clean
 fakeroot make -f debian/rules orig
@@ -140,9 +143,29 @@ fakeroot make -f debian/rules source
 fakeroot make -f debian/rules.gen setup_${ARCH}
 sed -i 's/binary-arch_arm64:: binary-arch_arm64_none binary-arch_arm64_real/binary-arch_arm64:: binary-arch_arm64_none/' debian/rules.gen
 sed -i 's/binary-arch_armhf:: binary-arch_armhf_extra binary-arch_armhf_none binary-arch_armhf_real/binary-arch_armhf:: binary-arch_armhf_extra binary-arch_armhf_none/' debian/rules.gen
-fakeroot make -f debian/rules.gen binary-arch_${ARCH}
+fakeroot make -f debian/rules.gen binary-arch_${ARCH} 2>&1 | tee LOG
 
 fi
 
 cd ..
+
+rm -f *-dbg_*.deb
+if test $CROSS = 0 ; then
+  L=kernel-amd64-$KVERR-1
+  mkdir -p $L
+  mv $CDIR/LOG *$KVERR*amd64.deb $L
+  tar cplf - $L | gzip -9 > $L.tar.gz
+  rm -fr $L
+else
+  L=kernel-rpi3-$ARCH-$KVERR-1
+  mkdir -p $L
+  mv $CDIR/LOG *$KVERR*$ARCH.deb $L
+  if test $ARCH = armhf ; then
+    mv *$KVERR*.udeb $L
+  fi
+  tar cplf - $L | gzip -9 > $L.tar.gz
+  rm -fr $L
+fi
+
+rm -fr $CDIR
 
