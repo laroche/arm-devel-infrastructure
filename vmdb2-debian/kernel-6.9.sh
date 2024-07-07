aac9b244f1b3cac0727b24cce7386583b2355c57
Florian La Roche <Florian.LaRoche@gmail.com>
2024-07-07 14:59:28 +0200

    update to upstream Linux kernel 6.9.8
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- vmdb2-debian/kernel-6.9.sh
+++ vmdb2-debian/kernel-6.9.sh
@@ -19,8 +19,8 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.9.7
-KVERR=6.9.7
+KVER=6.9.8
+KVERR=6.9.8
 CDIR=linux-$KVERR
 RVER=6.9.7
 
@@ -80,7 +80,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.9.7-/6.9.7-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.9.8-/6.9.8-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^bugfix/all/tipc-fix-UAF-in-error-path.patch,,g' $CDIR/debian/patches/series
