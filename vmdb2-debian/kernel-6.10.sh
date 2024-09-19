fb0c5aada9e08ad799ad5dd381d283a3530db409
Florian La Roche <Florian.LaRoche@gmail.com>
2024-09-19 13:21:28 +0200

    update to upstream Linux kernel 6.10.11

--- vmdb2-debian/kernel-6.10.sh
+++ vmdb2-debian/kernel-6.10.sh
@@ -19,8 +19,8 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.10.10
-KVERR=6.10.10
+KVER=6.10.11
+KVERR=6.10.11
 CDIR=linux-$KVERR
 RVER=6.10.10
 
@@ -79,7 +79,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.10.9-/6.10.10-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.10.10-/6.10.11-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^bugfix/all/net-drop-bad-gso-csum_start-and-offset-in-virtio_net.patch,,g' $CDIR/debian/patches/series
