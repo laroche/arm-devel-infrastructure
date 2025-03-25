09868cf34a57aa3c1390ab6d2a4b156a5f3beb89
Florian La Roche <Florian.LaRoche@gmail.com>
2025-03-25 06:28:21 +0100

    update to upstream Linux kernel 6.12.20
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- vmdb2-debian/kernel-6.12.sh
+++ vmdb2-debian/kernel-6.12.sh
@@ -19,8 +19,8 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.12.19
-KVERR=6.12.19
+KVER=6.12.20
+KVERR=6.12.20
 CDIR=linux-$KVERR
 RVER=6.12.19
 
@@ -79,7 +79,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.12.18-/6.12.19-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.12.20-/6.12.20-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^bugfix/arm64/phy-rockchip-naneng-combphy-compatible-reset-with-ol.patch,,g' $CDIR/debian/patches/series
