bf71606d5faa4dd75566a52b91d85c229af3f53c
Florian La Roche <Florian.LaRoche@gmail.com>
2025-05-23 13:53:44 +0200

    setup.sh: always remove kernel symlinks
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- vmdb2-debian/setup.sh
+++ vmdb2-debian/setup.sh
@@ -466,10 +466,10 @@ if test $SERVER = 0 -a -f /etc/sudoers ; then
 fi
 
 # Remove kernel/initrd symlinks.
-if test "X$SYSTYPE" != Xlxc -a ! -f /etc/kernel-img.conf -a -L /vmlinuz ; then
+if test ! -f /etc/kernel-img.conf ; then
   echo "do_symlinks = 0" > /etc/kernel-img.conf
-  rm -f /vmlinuz{,.old} /initrd.img{,.old}
 fi
+rm -f /vmlinuz{,.old} /initrd.img{,.old}
 
 # vim package updates overwrite this change, so we need to fix this periodically:
 # https://unix.stackexchange.com/questions/318824/vim-cutpaste-not-working-in-stretch-debian-9
