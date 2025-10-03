6cacc8455b4fb5925b4c66b95049d3bbc3babc96
Florian La Roche <Florian.LaRoche@gmail.com>
2025-10-03 08:52:04 +0200

    update to upstream Linux kernel 6.1.155, 6.12.50 and 6.16.10
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- vmdb2-debian/kernel.sh
+++ vmdb2-debian/kernel.sh
@@ -19,8 +19,8 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.1.154
-KVERR=6.1.154
+KVER=6.1.155
+KVERR=6.1.155
 CDIR=linux-$KVERR
 RVER=6.1.66
 
@@ -83,7 +83,7 @@ fi
 sed -i -e '/cplus_demangle/d' $CDIR/debian/rules.d/tools/perf/Makefile
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.1.153-/6.1.154-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.1.154-/6.1.155-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^bugfix/all/proc-fix-missing-pde_set_flags-for-net-proc-files.patch,,g' $CDIR/debian/patches/series
