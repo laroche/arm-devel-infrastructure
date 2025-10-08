565100eabf019476821de6f779e1ac07aef103e2
Florian La Roche <Florian.LaRoche@gmail.com>
2025-10-08 16:52:35 +0200

    update to upstream Linux kernel 6.12.51 and 6.16.11
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- vmdb2-debian/kernel-6.12.sh
+++ vmdb2-debian/kernel-6.12.sh
@@ -19,8 +19,8 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.12.50
-KVERR=6.12.50
+KVER=6.12.51
+KVERR=6.12.51
 CDIR=linux-$KVERR
 RVER=6.12.27
 
@@ -79,7 +79,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.12.49-/6.12.50-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.12.50-/6.12.51-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^bugfix/all/Revert-mmc-sdhci-Disable-SD-card-clock-before-changi.patch,,g' $CDIR/debian/patches/series
--- vmdb2-debian/kernel-6.16.sh
+++ vmdb2-debian/kernel-6.16.sh
@@ -19,10 +19,10 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.16.10
-KVERR=6.16.10
+KVER=6.16.11
+KVERR=6.16.11
 CDIR=linux-$KVERR
-RVER=6.16.9
+RVER=6.16.11
 
 CROSS=0
 ARCH=
@@ -68,7 +68,7 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
     popd
   fi
   cd $RDIR || exit 1
-  git format-patch -o ../rpi-patches-$RVER b0d8acc711981d712a59ee20b073cbc52a4109c5
+  git format-patch -o ../rpi-patches-$RVER 683320aeb0e83deac1b6c6794b0567969de09548
   cd ..
   rm -fr $RDIR
 fi
@@ -80,7 +80,7 @@ if ! test -d $CDIR ; then
 fi
 sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.16.9-/6.16.10-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.16.10-/6.16.11-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^bugfix/all/ext4-don-t-try-to-clear-the-orphan_present-feature-b.patch,,g' $CDIR/debian/patches/series
