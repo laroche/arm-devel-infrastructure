335b95bb3a20e58cfdfef3530b886b57bfd978f5
Florian La Roche <Florian.LaRoche@gmail.com>
2025-11-02 20:52:45 +0100

    update to upstream Linux kernel 6.12.57 and 6.17.7
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- vmdb2-debian/kernel-6.17.sh
+++ vmdb2-debian/kernel-6.17.sh
@@ -19,10 +19,10 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.17.6
-KVERR=6.17.6
+KVER=6.17.7
+KVERR=6.17.7
 CDIR=linux-$KVERR
-RVER=6.17.5
+RVER=6.17.6
 
 CROSS=0
 ARCH=
@@ -68,7 +68,7 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
     popd
   fi
   cd $RDIR || exit 1
-  git format-patch -o ../rpi-patches-$RVER 99efbd4259f384718dd16a7423d8e944396b65d4
+  git format-patch -o ../rpi-patches-$RVER 371f1e070fa95c71356104a406855a361aad5668
   cd ..
   rm -fr $RDIR
 fi
@@ -80,7 +80,7 @@ if ! test -d $CDIR ; then
 fi
 #sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.17.5-/6.17.6-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.17.6-/6.17.7-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^bugfix/all/docs-kdoc-handle-the-obsolescensce-of-docutils.Error.patch,,g' $CDIR/debian/patches/series
