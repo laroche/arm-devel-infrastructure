b59a7d7655699ed3e5a302c8158ccd0b91ac8e22
Florian La Roche <Florian.LaRoche@gmail.com>
2026-01-13 05:52:32 +0100

    update to upstream Linux kernel 6.12.65 and 6.18.5
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- vmdb2-debian/kernel-6.18.sh
+++ vmdb2-debian/kernel-6.18.sh
@@ -19,7 +19,7 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.18.4
+KVER=6.18.5
 KVERR=$KVER
 CDIR=linux-$KVERR
 RVER=6.18.4
@@ -80,7 +80,7 @@ if ! test -d $CDIR ; then
 fi
 #sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.18.3-/6.18.4-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.18.4-/6.18.5-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e 's,^bugfix/all/clk-samsung-exynos-clkout-Assign-.num-before-accessi.patch,,g' $CDIR/debian/patches/series
