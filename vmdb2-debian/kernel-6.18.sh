1e997a280d8c64a5e960d81ca3aebfcb2ea873e5
Florian La Roche <Florian.LaRoche@gmail.com>
2026-01-23 14:24:32 +0100

    update to upstream Linux kernel 6.1.161, 6.12.66 and 6.18.6
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- vmdb2-debian/kernel-6.18.sh
+++ vmdb2-debian/kernel-6.18.sh
@@ -19,10 +19,10 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.18.5
+KVER=6.18.6
 KVERR=$KVER
 CDIR=linux-$KVERR
-RVER=6.18.4
+RVER=6.18.6
 
 CROSS=0
 ARCH=
@@ -68,7 +68,7 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
     popd
   fi
   cd $RDIR || exit 1
-  git format-patch -o ../rpi-patches-$RVER 3aa9aac0e8b767a7c6fac33ae626a332c2ba1389
+  git format-patch -o ../rpi-patches-$RVER b6fe42bc55af3fb17c8c03def2f8a1f7fa907af6
   cd ..
   rm -fr $RDIR
 fi
@@ -80,7 +80,7 @@ if ! test -d $CDIR ; then
 fi
 #sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.18.4-/6.18.5-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.18.5-/6.18.6-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e 's,^bugfix/all/clk-samsung-exynos-clkout-Assign-.num-before-accessi.patch,,g' $CDIR/debian/patches/series
