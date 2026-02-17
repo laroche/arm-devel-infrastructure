208774babbdc7c1e80f073cb39620d4ce77ac95c
Florian La Roche <Florian.LaRoche@gmail.com>
2026-02-17 06:46:41 +0100

    update to upstream Linux kernel 6.12.73 and 6.18.12
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- vmdb2-debian/kernel-6.18.sh
+++ vmdb2-debian/kernel-6.18.sh
@@ -19,10 +19,10 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.18.10
+KVER=6.18.12
 KVERR=$KVER
 CDIR=linux-$KVERR
-RVER=6.18.9
+RVER=6.18.10
 
 CROSS=0
 ARCH=
@@ -68,7 +68,7 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
     popd
   fi
   cd $RDIR || exit 1
-  git format-patch -o ../rpi-patches-$RVER adb851edb70783e3ded28044491f5a3ed065b7b2
+  git format-patch -o ../rpi-patches-$RVER 41cec610f690603820c80c4871dbb55bec77b9a2
   cd ..
   rm -fr $RDIR
 fi
@@ -80,12 +80,12 @@ if ! test -d $CDIR ; then
 fi
 #sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.18.9-/6.18.10-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.18.11-/6.18.12-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
-sed -i -e 's,^bugfix/all/clk-samsung-exynos-clkout-Assign-.num-before-accessi.patch,,g' $CDIR/debian/patches/series
-sed -i -e 's,^bugfix/all/drm-amdgpu-don-t-attach-the-tlb-fence-for-SI.patch,,g' $CDIR/debian/patches/series
-sed -i -e 's,^bugfix/all/sched-proxy-Yield-the-donor-task.patch,,g' $CDIR/debian/patches/series
+#sed -i -e 's,^bugfix/all/clk-samsung-exynos-clkout-Assign-.num-before-accessi.patch,,g' $CDIR/debian/patches/series
+#sed -i -e 's,^bugfix/all/drm-amdgpu-don-t-attach-the-tlb-fence-for-SI.patch,,g' $CDIR/debian/patches/series
+#sed -i -e 's,^bugfix/all/sched-proxy-Yield-the-donor-task.patch,,g' $CDIR/debian/patches/series
 #sed -i -e 's,0001-net-tcp-dccp-prepare-for-tw_timer-un-pinning.patch,,g' $CDIR/debian/patches-rt/series
 #exit 0
 mkdir -p orig
