7540b5687102976536372ed601b3c9bb9a08ec7f
Florian La Roche <Florian.LaRoche@gmail.com>
2026-04-03 08:44:36 +0200

    update to upstream Linux kernel 6.12.80 and 6.19.11
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- vmdb2-debian/kernel-6.19.sh
+++ vmdb2-debian/kernel-6.19.sh
@@ -19,7 +19,7 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.19.10
+KVER=6.19.11
 KVERR=$KVER
 CDIR=linux-$KVERR
 RVER=6.19.10
@@ -74,13 +74,13 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
 fi
 
 if ! test -d $CDIR ; then
-  git clone --single-branch --depth 1 -b debian/latest https://salsa.debian.org/kernel-team/linux.git $CDIR
-  #git clone --single-branch --depth 1 -b debian/6.19/forky https://salsa.debian.org/kernel-team/linux.git $CDIR
+  #git clone --single-branch --depth 1 -b debian/latest https://salsa.debian.org/kernel-team/linux.git $CDIR
+  git clone --single-branch --depth 1 -b debian/6.19/forky https://salsa.debian.org/kernel-team/linux.git $CDIR
   #git clone --single-branch --depth 1 -b 6.19-stable-updates https://salsa.debian.org/carnil/linux.git $CDIR
 fi
 #sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.19.6-/6.19.10-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.19.10-/6.19.11-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^bugfix/rpi/0001-NFSD-Defer-sub-object-cleanup-in-export-put-callback.patch,,g' $CDIR/debian/patches/series
