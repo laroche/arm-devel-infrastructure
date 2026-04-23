47e0e20bedaa9df7098eb5a7be7e179c956cf927
Florian La Roche <Florian.LaRoche@gmail.com>
2026-04-23 09:55:47 +0200

    update to upstream Linux kernel 6.12.83 and 6.19.14
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- vmdb2-debian/kernel-6.19.sh
+++ vmdb2-debian/kernel-6.19.sh
@@ -19,10 +19,10 @@ if test "X$HOSTTYPE" != "Xx86_64" ; then
   RPIPATCHES=1
 fi
 
-KVER=6.19.13
+KVER=6.19.14
 KVERR=$KVER
 CDIR=linux-$KVERR
-RVER=6.19.12
+RVER=6.19.14
 
 CROSS=0
 ARCH=
@@ -68,7 +68,7 @@ if test "$RPIPATCHES" = 1 -a ! -d rpi-patches-$RVER ; then
     popd
   fi
   cd $RDIR || exit 1
-  git format-patch -o ../rpi-patches-$RVER ad8a3ed1e9f18b16b979b0e7e4d767f7033d0c31
+  git format-patch -o ../rpi-patches-$RVER b9dbb4576bc8c69e87b4ca4caa2fb1c0db47d722
   cd ..
   rm -fr $RDIR
 fi
@@ -80,7 +80,7 @@ if ! test -d $CDIR ; then
 fi
 #sed -i -e '/install-rtla)/d' $CDIR/debian/rules.real
 # Change Debian source to new version:
-sed -i -e '1 s/6.19.12-/6.19.13-/' $CDIR/debian/changelog
+sed -i -e '1 s/6.19.13-/6.19.14-/' $CDIR/debian/changelog
 sed -i -e '1 s/unstable/UNRELEASED/' $CDIR/debian/changelog
 sed -i -e '1 s/experimental/UNRELEASED/' $CDIR/debian/changelog
 #sed -i -e 's,^bugfix/rpi/0001-NFSD-Defer-sub-object-cleanup-in-export-put-callback.patch,,g' $CDIR/debian/patches/series
