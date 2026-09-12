e7999b658fd25d1f62dfdf6bbcbc400e6b703f0b
Florian La Roche <Florian.LaRoche@gmail.com>
2026-09-12 23:00:49 +0200

    setup.sh: adjust base kernel for Debian 12
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- vmdb2-debian/setup.sh
+++ vmdb2-debian/setup.sh
@@ -1436,8 +1436,8 @@ if test "X$DEMOSETUP" = X1 ; then
     config_git_default
   fi
   # Remove old Linux kernel:
-  if test -d /lib/modules/6.1.0-47-amd64 ; then
-    dpkg -P linux-image-6.1.0-47-amd64
+  if test -d /lib/modules/6.1.0-50-amd64 ; then
+    dpkg -P linux-image-6.1.0-50-amd64
   fi
   if test -d /lib/modules/6.12.86+deb13-amd64 ; then
     dpkg -P linux-image-6.12.86+deb13-amd64
